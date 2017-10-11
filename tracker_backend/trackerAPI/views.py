34a5c252d205980f66b26fe83cbffa6744e4fb20
dheerajpille <dheerajpille@gmail.com>
2017-10-11 11:20:31 -0400

    Removed superuser/admin's ability to edit other User data

--- tracker_backend/trackerAPI/views.py
+++ tracker_backend/trackerAPI/views.py
@@ -1,112 +1,160 @@
-from rest_framework.permissions import AllowAny, IsAdminUser
+from django.contrib.auth.models import User
 from django.http import Http404
+
 from rest_framework import status
 from rest_framework.generics import ListAPIView
-from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny, IsAdminUser
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from django.contrib.auth.models import User
 from tracker_backend.trackerAPI.serializers import UserSerializer, LoginSerializer, SignupSerializer
 
 
 class LoginView(APIView):
     """
-    Allows client to login with user credentials (username/password)
+    Logs client in with required User credentials (username/password)
     """
 
     # Gives any user permission to POST for login
     permission_classes = {AllowAny, }
 
+    # POST data for login request
     def post(self, request):
+
+        # Gets data from POST call and places it in LoginSerializer
         validate_user = LoginSerializer(data=request.data)
 
+        # Checks if given data is valid
         if validate_user.is_valid():
+
+            # Places the aforementioned data in UserSerializer
             serializer = UserSerializer(validate_user.validated_data, context={'request': request})
 
+            # Logs User into API
             return Response(serializer.data, status=status.HTTP_200_OK)
-        else:
-            return Response(validate_user.errors, status=status.HTTP_401_UNAUTHORIZED)
+
+        # Returns any errors found in POST data
+        return Response(validate_user.errors, status=status.HTTP_401_UNAUTHORIZED)
 
 
 class SignupView(APIView):
     """
-    Allows client to signup with user credentials (username/email/password)
-    Fields can be partial
+    Signs client up with required User credentials (username/email/password)
+    Partial input of other User fields allowed
     """
 
     # Gives any user permission to POST for signup
     permission_classes = {AllowAny, }
 
+    # POST data for signup request
     def post(self, request):
+
+        # Gets data from POST call and places it in SignupSerializer
         create_user = SignupSerializer(data=request.data)
 
+        # Checks if given data is valid
         if create_user.is_valid():
+
             create_user.save()
+
+            # Places the aforementioned data in UserSerializer
             serializer = UserSerializer(create_user.data, context={'request': request})
 
+            # Creates new User object in database
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(create_user.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        # Returns any errors found in POST data
+        return Response(create_user.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserDetail(APIView):
     """
-    Allows client to view, update, and delete their own data after authentication
+    Allows client to view, update, and delete their own User data after authentication
     """
 
-    # Obtains object in question via pk/id value
+    # Gets User object via pk/id value
+    # Returns user or 404, if not found
     def get_object(self, pk):
         try:
             return User.objects.get(pk=pk)
         except User.DoesNotExist:
             raise Http404
 
-    # Get the User object
+    # Gets the current User object
     def get(self, request, pk):
-        user = self.get_object(pk)
-        if request.user.is_superuser or request.user == user:
-            serializer = UserSerializer(user, context={'request', request})
-            return Response(serializer.data)
-        else:
-            return Response(data={"message": "Not authorized to view this user."}, status=status.HTTP_401_UNAUTHORIZED)
 
-    # Update the User object
-    def put(self, request, pk):
+        # Finds current User in database
         user = self.get_object(pk)
-        if request.user.is_superuser or request.user == user:
+
+        # Checks if the user sending the request is the user in question
+        if request.user == user:
+
+            # Serializes User data to UserSerializer
+            serializer = UserSerializer(user, context={'request', request})
+
+            # Returns User's data as response
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        # Returns when unauthorized access to User's data occurs
+        return Response(data={"message": "Not authorized to view this user."}, status=status.HTTP_401_UNAUTHORIZED)
+
+    # Updates the current User object
+    # Partial input of User fields allowed
+    def put(self, request, pk):
+
+        # Finds current User in database
+        user = self.get_object(pk)
+
+        # Checks if the user sending the request is the user in question
+        if request.user == user:
+
+            # Serializes User data to UserSerializer
             serializer = UserSerializer(user, data=request.data, partial=True, context={'request': request})
+
+            # Checks if given data from PUT call is valid
             if serializer.is_valid():
+
                 serializer.save()
 
-                return Response(serializer.data)
+                # Saves the updated User information in database
+                return Response(serializer.data, status=status.HTTP_200_OK)
 
+            # Returns errors that occur with PUT call data
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+        # Returns when unauthorized access to User's data occurs
         return Response(data={"message": "Not authorized to edit this user."}, status=status.HTTP_401_UNAUTHORIZED)
 
     # Delete the User object
     def delete(self, pk):
+
+        # Finds current User in database
         user = self.get_object(pk)
 
-        if request.user.is_superuser or request.user == user:
+        # Checks if the user sending the request is the user in question
+        if request.user == user:
+
+            # Deletes User object from database
             user.delete()
 
             return Response(status=status.HTTP_204_NO_CONTENT)
 
+        # Returns when unauthorized access to User's data occurss
         return Response(data={"message": "Not authorized to delete this user."}, status=status.HTTP_401_UNAUTHORIZED)
 
 
 class UserList(ListAPIView):
     """
-    Allows admin to view all users in database
+    Allows superuser/admin to view all Users in database
     """
 
     # Disables pagination for GET calls
     pagination_class = None
 
-    # Gives list-viewing access to superuser/admin accounts only
+    # Gives User list access to superuser/admin accounts only
     permission_classes = {IsAdminUser, }
 
+    # Specifies serializer class
     serializer_class = UserSerializer
 
     # Orders queryset of user accounts by pk/id value
--- tracker_backend/urls.py
+++ tracker_backend/urls.py
@@ -1,18 +1,3 @@
-"""tracker_backend URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url, include
 from django.contrib import admin
 
@@ -22,11 +7,12 @@ urlpatterns = [
     # Admin URL for Django
     url(r'^admin/', admin.site.urls),
 
-    # Login and Signup calls for Tracker
+    # Login and Signup calls for project
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^signup/$', SignupView.as_view(), name='signup'),
 
     # OAuth 2.0 verification for Django
+    # Primarily used for o/token/ or o/applications/
     url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
 
     # URL for project's resources
