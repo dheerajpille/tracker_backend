6862be42f8751f927ab00bc379757b932f9d6a8e
dheerajpille <dheerajpille@gmail.com>
2017-11-04 14:00:29 -0400

    Reworking STATIC_ROOT

--- tracker_backend/settings.py
+++ tracker_backend/settings.py
@@ -162,7 +162,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 STATIC_URL = '/static/'
 
 # OAuth ID and SECRET derived from /o/applications
