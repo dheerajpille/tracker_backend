1854e49828b3d7c4b82f715f0a1327797c372664
dheerajpille <dheerajpille@gmail.com>
2017-11-26 15:38:04 -0500

    Configuring grant types

--- tracker_backend/settings.py
+++ tracker_backend/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
 OAUTH2_PROVIDER = {
     # List of available scopes
     'SCOPES': {'read': 'Read scope', 'write': 'Write scope'}
+    'OAUTH2_BACKEND_CLASS': 'oauth2_provider.oauth2_backends.JSONOAuthLibCore'
 }
 
 # Django REST Framework settings
